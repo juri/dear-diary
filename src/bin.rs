efdf588041fdaeb85e466da54e2134ec7f504e7c
Juri Pakaste <juri@juripakaste.fi>
2020-07-08 16:21:40 +0300

    Read path from command line, fall back on a good default
    Two parameters: diary directory and diary name. First defaults to
    platform specific project directory, the second to "default".

--- src/bin.rs
+++ src/bin.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 
+mod diarydir;
 
 use clap::{App, Arg};
 use diary_core::{self, Diary, DiaryEntryKey};
+use std::path::PathBuf;
 use std::process;
 
 pub fn main() {
@@ -10,14 +12,33 @@ pub fn main() {
         .version("0.1.0")
         .author("Juri Pakaste <juri@juripakaste.fi>")
         .about("Manages diaries")
-        .arg(Arg::with_name("path")
-            .short("p")
-            .long("path")
-            .value_name("PATH")
-            .help("Location of the diary directory")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .value_name("PATH")
+                .help("Location of the diary directory")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("name")
+                .short("n")
+                .long("name")
+                .value_name("DIARY_NAME")
+                .help("Name of the diary")
+                .takes_value(true),
+        )
         .get_matches();
-
+    let mut path = matches
+        .value_of("path")
+        .map(PathBuf::from)
+        .or_else(|| diarydir::default_dir())
+        .unwrap_or_else(|| {
+            eprintln!("Couldn't determine diary root directory");
+            process::exit(1)
+        });
+    path.push(matches.value_of("name").unwrap_or("default"));
+    println!("path: {:?}", path);
     let diary = CLIDiary::open();
     diary.list_dates().first().map(|d| diary.show_entry(d));
 }
